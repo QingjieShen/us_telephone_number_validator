cab3c99bbfdae4843e84f626f13a7a44a5fdc2fe
Qingjie Shen (Clark) <shen0126@algonquinlive.com>
2024-01-21 19:28:26 -0500

    adds resultOutput function and change regex expression

--- js/main.js
+++ js/main.js
@@ -3,8 +3,30 @@ const result = document.getElementById("results-div");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 
-const countryCodeRegex = /(?:^|\s)1(?:\s)(?:\s|-|()(\d\d\d)(?:\s|)|-)(?:\s)(\d\d\d)(?:\s|)|-)(?:\s)(\d\d\d\d)(?:$|\s)/;
-const nromalNumberRegex = /(?:^|\s|()(\d\d\d)(?:\s|)|-)(?:\s)(\d\d\d)(?:\s|)|-)(?:\s)(\d\d\d\d)(?:$|\s)/
+const countryCodeRegex = /1\s?[\s-]?[(]?(\d\d\d)[)-]?[\s-]?(\d\d\d)[\s-]?(\d\d\d\d)/;
+const nromalNumberRegex = /^[\s-]?[(]?(\d\d\d)[)-]?[\s-]?(\d\d\d)[\s-]?(\d\d\d\d)$/;
 
 const regexs = [countryCodeRegex, nromalNumberRegex];
 
+const isValid = (msg) => regexs.some((regex) => regex.test(msg));
+const resultOutput = () => {
+    if (userInput.value === "") {
+        alert("Please provide a phone number");
+        return;
+    }
+    testResult = isValid(userInput.value) ?  `<p class="valid">Valid US number: ${userInput.value}</p>` : `<p class="invalid">Invalid US number: ${userInput.value}</p>`;
+    result.innerHTML += testResult;
+    userInput.value = "";
+}
+
+checkBtn.addEventListener("click", resultOutput)
+
+clearBtn.addEventListener("click", () => {
+    result.innerHTML = "";
+})
+
+userInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        resultOutput();
+    }
+})
